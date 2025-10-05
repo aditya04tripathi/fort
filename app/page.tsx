127bf789d68a94e6fda50d20272539fcc6732619
Aditya Tripathi <atri0048@student.monash.edu>
2025-10-06 00:04:50 +1100

    refactor: remove firebase export from index

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import React, { Suspense } from "react";
 export default function Home() {
   return (
     <Suspense fallback={<LoadingComponent />}>
-      <h1>Hello World</h1>
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1>WIP on Fortress!</h1>
+      </div>
     </Suspense>
   );
 }
